ab9e0dfa2d6698bf7870a940c60a36b7781cec3f
RamKoirala1123 <69522882+RamKoirala1123@users.noreply.github.com>
2024-03-25 07:40:39 +0545

    final changesss

--- sweetstore/settings.py
+++ sweetstore/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ucrwg_ut2w1t9xplzj^(zg#308$vftl(+_gpo%iv6cnm$=38nz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app", ".now.sh","127.0.0.1"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -118,9 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = BASE_DIR/'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'app','static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static') 
+STATIC_ROOT = BASE_DIR/'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
