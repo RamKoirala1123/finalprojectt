6f88540f5a383ba82ef42897c15183ef81c16b56
RamKoirala1123 <69522882+RamKoirala1123@users.noreply.github.com>
2024-03-25 12:17:50 +0545

    cords

--- sweetstore/settings.py
+++ sweetstore/settings.py
@@ -136,3 +136,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # mongodb+srv://bibek:<bibek@1357>@sweetstore.wq4umeo.mongodb.net/
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://finalprojectt-production.up.railway.app",
+]
